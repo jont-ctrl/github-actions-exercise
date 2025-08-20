767dc377bfdd48745eb59e5e6f0b1624d737fe19
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-08-20 11:45:11 +0200

    react init

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello world</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dicta
+        provident officiis fuga, sequi molestias alias tempora asperiores
+        corporis, est assumenda saepe quisquam modi maxime accusantium
+        voluptatibus, consectetur culpa aperiam maiores.
+      </p>
+    </>
+  );
+}
+
+export default App;
